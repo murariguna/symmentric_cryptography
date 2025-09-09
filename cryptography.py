ab04037999f5cc4e069f67e4f8e591d21df104b2
murariguna <murariguna3939@gmail.com>
2025-09-09 09:19:28 +0530

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -3,3 +3,15 @@ def monoalphabatic_cipher(message,key):
 def polyalphabatic_cipher(message,key):
 def play_fair_cipher(message,key):
 def hill_cipher(message,key):
+#inputs
+ 
+# Take user input for the message
+message = input("Enter the Message: ")
+
+# Display cipher options
+print("Select a cipher type:")
+print("1. Caesar Cipher")
+print("2. Monoalphabetic Cipher")
+print("3. Polyalphabetic Cipher")
+print("4. Playfair Cipher")
+print("5. Hill Cipher")
