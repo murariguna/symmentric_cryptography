3c05cf5ca2c35f42290b4cabdb1aae7b6baf3d0c
murariguna <murariguna3939@gmail.com>
2025-09-09 09:51:55 +0530

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -1,5 +1,36 @@
-def ceaser_cipher(message,key):
+def ceaser_cipher(message):
+    # Initialize the encrypted message as an empty string
+    encrypted_message = ""
+    #We use the ord() function to convert characters to their ASCII values and chr() to convert them back.
+    for char in message:
+        if char.islower(): 
+            new_position = (ord(char) - ord('a') + 3) % 26
+            encrypted_message += chr(new_position + ord('a'))
+        elif char.isupper():  
+            new_position = (ord(char) - ord('A') + 3) % 26
+            encrypted_message += chr(new_position + ord('A'))
+        else:
+            encrypted_message += char
+    
+    # Output the encrypted message
+    print("Encrypted message:", encrypted_message)
+
 def monoalphabatic_cipher(message,key):
+     # Initialize the encrypted message as an empty string
+    encrypted_message = ""
+    #We use the ord() function to convert characters to their ASCII values and chr() to convert them back.
+    for char in message:
+        if char.islower(): 
+            new_position = (ord(char) - ord('a') + key) % 26
+            encrypted_message += chr(new_position + ord('a'))
+        elif char.isupper():  
+            new_position = (ord(char) - ord('A') + key) % 26
+            encrypted_message += chr(new_position + ord('A'))
+        else:
+            encrypted_message += char
+    
+    # Output the encrypted message
+    print("Encrypted message:", encrypted_message)
 def polyalphabatic_cipher(message,key):
 def play_fair_cipher(message,key):
 def hill_cipher(message,key):
@@ -15,3 +46,17 @@ print("2. Monoalphabetic Cipher")
 print("3. Polyalphabetic Cipher")
 print("4. Playfair Cipher")
 print("5. Hill Cipher")
+#Take user input for cipher
+choice = int(input("Enter your choice (1-5): "))
+
+if choice == 1:
+    print("Caesar Cipher selected....")
+     # Get the shift key from the user
+    print("THE KEY IS ALWAYS 3 IN CAESAR CIPHER")
+    ceaser_cipher(message)
+if choice == 2:
+   print("Monoalphabatic cipher is selcted....")
+   key = int(input("Enter the shift(0-26): "))
+   
+    
+    
